c3c660a255fd71a52d28b09b75819c02108fb05a
balogunjamiu49@gmail.com <balogunjamiu49@gmail.com>
2021-08-18 05:57:50 +0100

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,138 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () =>(
+  <header className="header">
+        <div className="content-container">
+            <div className="header__content">
+                  <h1 className="header__title">My Resume</h1>
+                  <img  alt="HNG logo" className="header__title" src='https://www.logolynx.com/images/logolynx/s_f8/f87fd3b9b3fb411a37d90d3bf4bd9464.jpeg'/>
+            </div>
+            <button className="button"><a href="https://hng.tech/">HNG Website</a></button>
+              <h3 className="description-title"> BALOGUN JAMIU OLAMIDIPUPO</h3>
+              <p className="description-title">balogunmaiu@gmail.com| 09099545319 | no.balogunmohammed ave,ikola,ikeja,Lagos</p>
+              <p className="description-title">github:github.com/Balogunjamiu</p>
+              <div>
+                <h3 className="career-objectives">Career objective</h3>
+                <p className="career-objective-talk"> To be in a position where I can maximize my potential as a productive and active individual
+                      giving quality performance at all times for the attainments of the goal of the organization I
+                      become part of. I am committed to maintaining the organization’s standard and reputation. In
+                      addition, the world is innovation driven, with passion I will relentlessly contribute immensely to
+                      the greatness of the company
+                    </p>
+                    <h3 className="career-objectives">Personal data</h3>
+                    <div className="data">
+                      <p>Sex</p>
+                      <p>male</p>
+                    </div>
+                    <div className="data">
+                      <p>Date of Birth</p>
+                      <p>april, 14 2000</p>
+                    </div>
+                    <div className="data">
+                      <p>Marital status</p>
+                      <p>Single</p>
+                    </div>
+                    <div className="data">
+                      <p>State of origin</p>
+                      <p>Kwara</p>
+                    </div>
+                    <div className="data">
+                      <p>L.G.A</p>
+                      <p>Ilorin East</p>
+                    </div>
+                    <div className="data">
+                      <p>Nationality</p>
+                      <p>Nigeria</p>
+                    </div>
+                    <div className="data">
+                      <p>Language spoken</p>
+                      <p>English and Yoruba</p>
+                    </div>
+
+                    <h3 className="career-objectives_b">Education Backgrounds With Dates</h3>
+                    <div className="data_b">
+                      <p>National Diploma (ND)Kwara State Polythecnic</p>
+                      <p>2018 –2021</p>
+                    </div>
+                    <div className="data_b">
+                      <p>O’Level WASSCE (St. Lawrence Greater Heights College)</p>
+                      <p>2013-2016</p>
+                    </div>
+                    <div className="data_b">
+                      <p>Hope International Nur/primary school. </p>
+                      <p>2005-2010</p>
+                    </div>
+                    <h3 className="career-objectives_b">Software Development certificates</h3>
+                    <div className="data_b">
+                      <p>Nodejs certificate(UDEMY)</p>
+                      <p>2019-2020</p>
+                    </div>
+                    <div className="data_b">
+                      <p>Reactjs certificate(UDEMY)</p>
+                      <p>2019-2020</p>
+                    </div>
+                    <h3 className="career-objectives_b">Qualification Obtained With Dates</h3>
+                    <div className="data_b">
+                      <p>National Diploma</p>
+                      <p>2018-2021</p>
+                    </div>
+                    <div className="data_b">
+                      <p>Senor Secondary School Cerficate(SSCE)</p>
+                      <p>2016</p>
+                    </div>
+                    <div className="data_b">
+                      <p>First Leaving School certificate</p>
+                      <p>2010</p>
+                    </div>
+                    <h3 className="career-objectives_b">Personal skills</h3>
+                    <div className="data_b">
+                      <p>Highly organized and efficient</p>
+                    </div>
+                    <div className="data_b">
+                      <p>Computer literate</p>
+                    </div>
+                    <div className="data_b">
+                      <p>Excellent communication and interpretation</p>
+                    </div>
+                    <div className="data_b">
+                      <p>Ability to work independently or as a part of a team</p>
+                    </div>
+                    <h3 className="career-objectives_b">Work Experience</h3>
+                    <div className="data_b">
+                      <p>Zionwol Solution Limited</p>
+                      <p>2017-2018</p>
+                      </div>
+                      <h3 className="career-objectives_b">Hobbies/Interested</h3>
+                      <div className="data_b">
+                      <p>Coding</p>
+                    </div>
+                    <div className="data_b">
+                      <p>Reading</p>
+                    </div>
+                    <div className="data_b">
+                      <p>Making Researches</p>
+                    </div>
+                    <h3 className="career-objectives_b">Referee</h3>
+                    <div className="data_b">
+                      <p>Balogun Mohammed</p>
+                    </div>
+                    <div className="data_b">
+                      <p>08037189700</p>
+                    </div>
+                    <div className="data_b">
+                      <p>balogunmuhammed20@gmail.com</p>
+                    </div>
+
+
+
+
+
+
+
+                </div>    
+        </div>
+    </header>  
+)
+
+  export default Header
---
+++ src/context/notes-context.js
@@ -0,0 +1,4 @@
+import React from 'react'
+const NotesContext = React.createContext()
+
+export default NotesContext
